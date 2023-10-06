c8f650629baf0cc2013ef4335648223b7aa9f421
Nimo-123 <146323878+Nimo-123@users.noreply.github.com>
2023-10-07 00:43:16 +0300

    Add files via upload

---
+++ escapesaquence3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<<"   o    0              ______                                 "<<endl;
+cout<<"  /|\\  /8\\     ________/_\\_____                           "<<endl; 
+ cout<<"   |   /_\\     ||__--____--____|                        "<<endl; 
+cout<<" _/ \\_ _|_          ()    ()                            "<<endl;   
+                                             
+cout<<" Stick man and woman holding hands                           "<<endl;   
+                                             
+cout<<"  next to their car  "<<endl;   
+ 
+
+}
+
+
+
+
+
